178b241d44c4900e973724b55619510f36d61edb
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-02-21 23:14:43 +0530

    slider prepared

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -12,25 +12,25 @@ const Slider = ({ images }) => {
 
     return (
         <div className='flex w-[100vw]'>
-            <div>
-        <div className='w-full sm:w-[60%] xl:w-[50%] m-auto greyshadow pb-1 pt-6 md:px-8' style={{color:theme.bglightsecondary}}>
-            <Carousel
-            showArrows={true}
-            useKeyboardArrows={true}
-            emulateTouch={true}
-            width="100%"
-            labels={false}
-            onClickThumb={(e)=>setIsBorder(e)}
-            >
-                {images.map((item , i) => (
-                    <div className='shadow-xl h-[200px] md:h-[240px] xl:h-[350px]' style={{shadowColor:theme.bgdarkprimary}}>
-                        <img src={urlFor(item).url()}  className={`h-full ${isborder===i && 'border-2'} object-contain`} style={{borderColor:theme.bgdarkprimary}}/>
-                    </div>
-                ))}
-            </Carousel>
-        </div>
-        </div>
-        </div>
+      
+    <div className='w-full sm:w-[60%] xl:w-[50%] m-auto greyshadow pb-1 pt-6 md:px-8' style={{color:theme.bglightsecondary}}>
+        <Carousel
+        showArrows={true}
+        useKeyboardArrows={true}
+        emulateTouch={true}
+        width="100%"
+        labels={false}
+        onClickThumb={(e)=>setIsBorder(e)}
+        >
+            {images.map((item , i) => (
+                <div className='shadow-xl h-[200px] md:h-[240px] xl:h-[350px]' style={{shadowColor:theme.bgdarkprimary}}>
+                    <img src={urlFor(item).url()}  className={`h-full ${isborder===i && 'border-2'} object-contain`} style={{borderColor:theme.bgdarkprimary}}/>
+                </div>
+            ))}
+        </Carousel>
+    </div>
+    </div>
+  
     )
 }
 
