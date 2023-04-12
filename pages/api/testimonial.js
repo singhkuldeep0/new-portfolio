d942e585379a80dd7ee4d6d4996f3a135d8ce60b
kuldeep <singhkuldeep7850@gmail.com>
2023-04-12 15:49:59 +0530

    error sloved

--- pages/api/testimonial.js
+++ pages/api/testimonial.js
@@ -1,8 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
     try {
         const prisma = new PrismaClient()
 
@@ -38,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             res.status(200).json({ testimonial })
         }
 
-    } catch (error: any) {
+    } catch (error) {
         console.log(error)
         throw new Error(error)
     }
