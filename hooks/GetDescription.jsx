6cf716ad5acc96140bf67346680e1cdbaba15728
kuldeep <singhkuldeep7850@gmail.com>
2023-04-25 16:32:59 +0530

    files changes

--- hooks/GetDescription.jsx
+++ hooks/GetDescription.jsx
@@ -110,7 +110,70 @@ const getDescription = ({ projectName }) => {
 
         case "Airbnb-clone":
             return <div>
-                <h1>Airbnb-clone</h1>
+                <h1>This is a fullStack Airbnb Clone using Next.js 13 with Login Validation & Authentication where one can explore different places and also can filter places, can add in my trips , in reservations, favourites. You can explore your properties that you have uploaded
+                </h1>
+                <h1 className='mt-3 font-bold' style={{ fontSize: fontSize.lg }}>Packages Used :</h1>
+                <ul className='mt-2'>
+                  <li>• @next-auth/prisma-adapter</li>
+                  <li>• @prisma/client</li>
+                  <li>• axios</li>
+                  <li>• bcrypt</li>
+                  <li>• date-fns</li>
+                  <li>• next-auth</li>
+                  <li>• next-cloudinary</li>
+                  <li>• query-string</li>
+                  <li>• react-date-range</li>
+                  <li>• react-hook-form</li>
+                  <li>• react-hot-toast</li>
+                  <li>• react-icons</li>
+                  <li>• react-leaflet</li>
+                  <li>• react-select</li>
+                  <li>• react-spinners</li>
+                  <li>• typescript</li>
+                  <li>• world-countries</li>
+                  <li>• zustand</li>
+                  <li>• prisma</li>
+                  <li>• tailwindcss</li>
+                </ul>
+
+                <h1 className='mt-3 font-bold' style={{ fontSize: fontSize.lg }}>Things that can be improved :</h1>
+                <ul className='mt-2'>
+                    <li className='ml-3'>• live location map can be added</li>
+                </ul>
+            </div>
+
+        case "ShareFeed":
+            return <div>
+                <h1>This is a Photo sharing app with login , validation and authentication where one can upload photos and you can see comments and can filter pictures on the basis categories
+                </h1>
+                <h1 className='mt-3 font-bold' style={{ fontSize: fontSize.lg }}>Packages Used :</h1>
+                <ul className='mt-2'>
+                  <li>• @next-auth/prisma-adapter</li>
+                  <li>• @prisma/client</li>
+                  <li>• axios</li>
+                  <li>• bcrypt</li>
+                  <li>• date-fns</li>
+                  <li>• next-auth</li>
+                  <li>• next-cloudinary</li>
+                  <li>• query-string</li>
+                  <li>• react-date-range</li>
+                  <li>• react-hook-form</li>
+                  <li>• react-hot-toast</li>
+                  <li>• react-icons</li>
+                  <li>• react-leaflet</li>
+                  <li>• react-select</li>
+                  <li>• react-spinners</li>
+                  <li>• typescript</li>
+                  <li>• world-countries</li>
+                  <li>• zustand</li>
+                  <li>• prisma</li>
+                  <li>• tailwindcss</li>
+                </ul>
+
+                <h1 className='mt-3 font-bold' style={{ fontSize: fontSize.lg }}>Things that can be improved :</h1>
+                <ul className='mt-2'>
+                    <li className='ml-3'>• live location map can be added</li>
+                </ul>
             </div>
 
         default:
